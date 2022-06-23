4ac3c6443c33fc8bc40b07532e36cce2864b2f71
Isabel N <isanpe@gmail.com>
2022-06-23 17:33:14 -0400

    Segundo commit config blog

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def create
+    @post = Post.create(
+      title: params[:title],
+      image: params[:image],
+      content: params[:content]
+    )
+  end
+
+end
